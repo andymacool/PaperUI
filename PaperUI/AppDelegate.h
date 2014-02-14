28bcc0089c74953726b0ad8e453c8ad5edf9047e
Andy <andy@upthere.com>
2014-02-14 14:23:32 -0800

    Initial Commit

---
+++ PaperUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PaperUI
+//
+//  Created by Andy Wang on 2/14/14.
+//  Copyright (c) 2014 Andy Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
